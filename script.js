9ee12068b6c84c3138a4d7201ec24cb9a157574c
DanielaPereira07 <164253863+DanielaPereira07@users.noreply.github.com>
2024-07-05 16:03:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+<script>
+     alert( "Olá!!! Você acabou de entrar no site da ALBANO IMÓVEIS.");
+     var nome = window.prompt ("Qual seu nome?")
+     window.alert(" Seja bem-vindo" + nome + " !")
+</script>
